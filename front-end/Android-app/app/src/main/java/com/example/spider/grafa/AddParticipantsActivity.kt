708f9ba67646322f9bb86f6574a38dbd9de7d11c
Kevinlega <kevinleglez@gmail.com>
2018-10-30 18:36:19 -0400

    Android app layout is done

--- front-end/Android-app/app/src/main/java/com/example/spider/grafa/AddParticipantsActivity.kt
+++ front-end/Android-app/app/src/main/java/com/example/spider/grafa/AddParticipantsActivity.kt
@@ -3,11 +3,10 @@ package com.example.spider.grafa
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginMainActivity : AppCompatActivity() {
+class AddParticipantsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_main)
-
+        setContentView(R.layout.activity_add_participants)
     }
 }
