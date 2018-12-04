ad15371a81cc425554560bd7e0fcdd9ab83106c3
Kevinlega <kevinleglez@gmail.com>
2018-12-04 18:40:51 -0400

    David and Kevin Login Activity Done

--- front-end/Android-app/Grafia/app/src/main/java/com/example/spider/grafia/LoginActivity.kt
+++ front-end/Android-app/Grafia/app/src/main/java/com/example/spider/grafia/LoginActivity.kt
@@ -1,44 +1,41 @@
 package com.example.spider.grafia
 
+import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 import java.lang.Exception
 import java.lang.StringBuilder
+import java.net.URL
 import java.security.MessageDigest
 
 class LoginActivity : AppCompatActivity() {
 
-    private fun byteArrayToHexString(array: Array<Byte>): String{
-
-        var result = StringBuilder(array.size * 2)
-
-        for (byte in array){
-            val toAppend = String.format("%2X", byte).replace(" ","0")
-            result.append(toAppend)
+    // Checks if an email is already registered.
+    private fun isRegistered(email:String,password:String){
+        val query = 4
+        val connectToAPI = Connect(this, 0,email,password)
+        try{
+            val url = "http://54.81.239.120/selectAPI.php?queryType=$query&email=$email"
+            println(url)
+            connectToAPI.execute(url)
         }
-        result.setLength(result.length)
-
-        return result.toString()
+        catch (error: Exception){}
     }
+    private fun checkLogin(password:String, email:String): Boolean{
+        var canLogin = true
+        if(password.isNullOrBlank() || email.isNullOrBlank()){
+            canLogin = false
+            Toast.makeText(this, "All Fields are Required.", Toast.LENGTH_LONG).show()
+        }
 
-    private fun md5(data: String):String {
-
-        var result = ""
-
-        try {
-
-            val md5 = MessageDigest.getInstance("MD5")
-            val md5HashBytes = md5.digest(data.toByteArray()).toTypedArray()
-
-            result = byteArrayToHexString(md5HashBytes)
-
-        }catch (e: Exception){}
-
-        return result
+        return canLogin
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,12 +44,22 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar!!.setTitle("Grafía Login")
 
         loginButton.setOnClickListener {
+
+
+            val Email = findViewById<EditText>(R.id.loginEmail)
+            val Password = findViewById<EditText>(R.id.loginPassword)
+            val email = Email.text.toString()
+            val password = Password.text.toString()
+            if (checkLogin(password,email)) {
+                isRegistered(email, password)
+            }
+
 //            if verified
-            val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+//            val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
             // To pass any data to next activity
 //            intent.putExtra("keyIdentifier", value)
 //             start your next activity
-            startActivity(intent)
+//            startActivity(intent)
 //            else
 //            val intent = Intent(this@LoginActivity,NotVerifiedActivity::class.java)
             // To pass any data to next activity
@@ -74,6 +81,110 @@ class LoginActivity : AppCompatActivity() {
 //            intent.putExtra("keyIdentifier", value)
 //             start your next activity
             startActivity(intent)
+        }
+    }
+
+
+    companion object {
+        class Connect(private val mContext: Context,private val flag: Int, private val email: String, private val password: String) :
+            AsyncTask<String, Void, String>() {
+
+
+            private fun getUID(email:String){
+                val query = 2
+                val connectToAPI = LoginActivity.Companion.Connect(mContext, 1,email,"")
+                try{
+                    val url = "http://54.81.239.120/selectAPI.php?queryType=$query&email=$email"
+                    println(url)
+                    connectToAPI.execute(url)
+                }
+                catch (error: Exception){}
+            }
+
+            private fun byteArrayToHexString(array: Array<Byte>): String {
+
+                var result = StringBuilder(array.size * 2)
+
+                for (byte in array) {
+                    val toAppend = String.format("%2X", byte).replace(" ", "0")
+                    result.append(toAppend)
+                }
+                result.setLength(result.length)
+
+                return result.toString()
+            }
+
+            private fun md5(data: String): String {
+
+                var result = ""
+
+                try{
+
+                    val md5 = MessageDigest.getInstance("MD5")
+                    val md5HashBytes = md5.digest(data.toByteArray()).toTypedArray()
+
+                    result = byteArrayToHexString(md5HashBytes)
+
+                } catch (e: Exception) {
+                }
+
+                return result
+            }
+
+            override fun doInBackground(vararg p0: String?): String {
+                return downloadJSON(p0[0])
+            }
+
+            private fun downloadJSON(url: String?): String {
+                return URL(url).readText()
+            }
+
+            override fun onPostExecute(result: String) {
+                try {
+                    val jSONObject = JSONObject(result)
+                    println(jSONObject)
+
+                    if(flag == 0){
+                        val registered = jSONObject.getBoolean("empty")
+
+                        if (!registered) {
+
+                            val dbPassword = jSONObject.getString("hashed_password")
+                            val dbSalt = jSONObject.getString("salt")
+
+                            val tryPassword = saltAndHash(password,dbSalt)
+                            println(tryPassword)
+                            if (tryPassword == dbPassword) {
+                                getUID(email)
+                            }
+                        } else {
+                            Toast.makeText(mContext, "Incorrect login info, try again.", Toast.LENGTH_SHORT).show()
+                        }
+                    } else{
+                        val uid = jSONObject.getInt("uid")
+                        val verified = jSONObject.getInt("verified")
+
+                        if (verified == 1) {
+                            val intent = Intent(mContext, DashboardActivity::class.java)
+                            intent.putExtra("userId", uid)
+                            mContext.startActivity(intent)
+                        } else{
+                            val intent = Intent(mContext, NotVerifiedActivity::class.java)
+                            mContext.startActivity(intent)
+                        }
+                    }
+
+                } catch (error: Exception) {
+                }
+                super.onPostExecute(result)
+            }
+
+            // Generates a salt and hashes a function
+            private fun saltAndHash(password:String,salt:String): String{
+                val salted = password + salt
+                return md5(salted).toLowerCase()
+            }
+
         }
     }
 }
